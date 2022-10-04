5da3e0e38837e9369e8c0895d01f6959109d23ae
Nicholas Long <nicholas.eden.long@gmail.com>
2022-10-04 13:56:12 +0000

    cleanup

--- find-valid.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find zet -name raw.md | xargs ~/kb/awk-scripting/get-headings.awk | awk '
-seen[$1] { dead[$1] = 1 }
-{ seen[$1] = 1 }
-END {
-  for (fn in seen ) if (!dead[fn]) print fn
-}
-'
