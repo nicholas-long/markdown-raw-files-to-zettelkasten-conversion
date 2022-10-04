ad1b617367bad005e4c948bbbc072b5ba50e504d
Nicholas Long <nicholas.eden.long@gmail.com>
2022-10-04 04:24:34 +0000

    script to add tags

---
+++ add-tag.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# parse args with case statement
+VERBOSE=0
+PAGING="no"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -t|--tag)
+      shift # pop arg
+      tag="$1"
+      tag="${tag//#/}"
+      ;;
+    -h|--help)
+      echo "Usage: $0 [ options ] directory"
+      echo "Options:"
+      cat "$0" | grep '^\s\+-.|--.*'
+      exit 1
+      ;;
+    *)
+      arg="$1"
+      echo "Add tag $tag to $arg"
+      cat zet/$arg/README.md
+      filename="zet/$arg/README.md"
+      nvim -c ":execute \"normal! GA #$tag\<esc>\"" -c ':wq' $filename
+      ;;
+  esac
+  shift
+done
